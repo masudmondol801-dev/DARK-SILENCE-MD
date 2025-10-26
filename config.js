ee8f89cad92a04716a3f27f7dedb14419dac3bac
masudmondol801-dev <masudmondol801@gmail.com>
2025-10-26 14:36:31 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~uQsxCDgC#Grb15P4ZIG0FxIwFhWVpLxI28LoMEqKGH-UL3EgBrCU",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
